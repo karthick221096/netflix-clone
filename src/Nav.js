2f5112d082bc7277a11dc8cf476bdb2571a609d8
karthick221096 <karthickr221096@gamil.com>
2022-07-26 21:03:13 +0530

    nav bar updated

--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from "react";
-import './Nav.css'
+import "./Nav.css";
 
 function Nav() {
-    // const [show , handleShow] = useState(false)
-    // useEffect(() =>{
-    //     window.addEventListener("scroll", ()=>{
-    //         if(window.scrollY > 100) {
-    //             handleShow(true)
-    //         } else handleShow(false)
-    //     })
-    //     return () => {
-    //         window.removeEventListener("scroll")
-    //     }
-    // }, [])
-    const show = false
+  const [show, handleShow] = useState(false);
+  // useEffect(() =>{
+  //     window.addEventListener("scroll", ()=>{
+  //         if(window.scrollY > 100) {
+  //             handleShow(true)
+  //         } else handleShow(false)
+  //     })
+  //     return () => {
+  //         window.removeEventListener("scroll")
+  //     }
+  // }, [])
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll", handleShow);
+    };
+  }, []);
 
   return (
     <div className={`nav ${show && "nav_black"}`}>
